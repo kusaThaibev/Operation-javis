34ef5ec8c039c25fb1e726c6a99d930430699cfc
boss <kusa.thua@gmail.com>
2024-05-24 12:52:24 +0700

    test555

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+    for i in range(0,100):
+        print(i)
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
